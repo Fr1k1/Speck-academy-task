991da079906775d6b11c743ce0aa0960ca51e54e
Fr1k1 <martin.friscic6@gmail.com>
2023-05-04 19:50:06 +0200

    Added sign in form

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import CoursesPage from "./Pages/Courses/CoursesPage"
 import Course_page from "./Pages/Course_page/Course_page"
 import SignIn from "./Pages/SignIn/SignIn"
 import Register from "./Pages/Register/Register"
-import  Header  from "./Components/Header/Header"
+import Header from "./Components/Header/Header"
 
 
 function App() {
@@ -12,45 +12,45 @@ function App() {
   return (
     <>
 
-    <Header/>
+      <Header />
 
-    <main>
+      <main>
 
-      <Routes>
+        <Routes>
 
-        <Route path="/"
-
-
-          element={
-
-            <Homepage />
-          }
+          <Route path="/"
 
 
-        >
+            element={
 
-        </Route>
-
-        <Route path="/courses"
-
-          element={
-
-            <CoursesPage />
-          }
-
-        >
+              <Homepage />
+            }
 
 
-        </Route>
+          >
 
-        <Route path={"/courses/:title"} element={<Course_page/>} />
+          </Route>
 
-        <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/courses"
 
-        <Route path="/register" element={<Register/>}/>
+            element={
+
+              <CoursesPage />
+            }
+
+          >
 
 
-      </Routes>
+          </Route>
+
+          <Route path={"/courses/:title"} element={<Course_page />} />
+
+          <Route path="/login" element={<SignIn />} />
+
+          <Route path="/register" element={<Register />} />
+
+
+        </Routes>
 
       </main>
     </>
--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import { Formik } from "formik";
+import Section from "../../components/Section/Section";
+import * as Yup from "yup";
+import {
+    Button,
+    ErrorMessage,
+    Field,
+    Form,
+    FormRow,
+    Option,
+    Select,
+} from "../../utils/styles/generalStyles";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+    return (
+        <Section title="Sign in">
+            <Formik
+                initialValues={{
 
-export default SignIn
+                    email: "",
+                    password: "",
+
+                }}
+                validationSchema={Yup.object({
+
+                    email: Yup.string()
+                        .email("Email is not valid!")
+                        .required("Email is required!"),
+                    password: Yup.string()
+                        .min(8, "Password must be at least 8 characters long")
+                        .required("Password is required!"),
+
+                })}
+                onSubmit={(values, { setSubmitting, resetForm }) => {
+                    console.log(values);
+                    // alert(JSON.stringify(values, null, 2));
+                    setTimeout(() => {
+                        const data = {
+
+                            email: values.email,
+                            password: values.password,
+
+
+                        };
+
+                        alert(JSON.stringify(data, null, 2));
+                        setSubmitting(false); // da je zavrsilo
+                        resetForm();
+                    }, 1000);
+                }}
+            >
+                {(formik) => (
+                    // tu sad slazemo svoju formu
+
+                    <Form>
+
+
+
+
+                        <FormRow>
+                            <Field
+                                type="email"
+                                name="email"
+                                placeholder="Email..."
+                                disabled={formik.isSubmitting}
+                            />
+                            <ErrorMessage
+                                name="email"
+                                component={"div"} // component div je da se preuzme i css error teksta
+                            />
+                        </FormRow>
+
+                        <FormRow>
+                            <Field
+                                type="password"
+                                name="password"
+                                placeholder="Password..."
+                                disabled={formik.isSubmitting}
+                            />
+                            <ErrorMessage
+                                name="password"
+                                component={"div"} // component div je da se preuzme i css error teksta
+                            />
+                        </FormRow>
+
+
+
+
+
+                        <FormRow>
+                            <Button isSecondary type="submit" disabled={formik.isSubmitting}>
+                                {formik.isSubmitting ? "Processing..." : "SignIn"}
+                            </Button>
+                        </FormRow>
+                    </Form>
+                )}
+            </Formik>
+        </Section>
+    );
+};
+
+export default SignIn;
